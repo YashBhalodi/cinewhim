c1c0a387c489a9b90f6a8ffda65eb7457a1d97bb
yashbhalodi-toddleapp <yashbhalodi@toddleapp.com>
2024-07-28 18:44:16 +0530

    fix: horizontal mode for movie card feed component

--- components/MovieCardFeed.tsx
+++ components/MovieCardFeed.tsx
@@ -2,22 +2,43 @@ import { Movie } from "@/api/model";
 import MovieCard from "@/components/MovieCard";
 import { Link } from "expo-router";
 import { useCallback } from "react";
-import { FlatList, ListRenderItem, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  FlatListProps,
+  ListRenderItem,
+  StyleSheet,
+  View,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ThemedView } from "./ThemedView";
 
-interface MovieCardFeedProps {
+interface MovieCardFeedProps
+  extends Omit<FlatListProps<Movie>, "data" | "renderItem"> {
   movies: Movie[];
-  onLoadMore: () => void;
+  onLoadMore?: () => void;
   isLoadingMore?: boolean;
+  isHorizontal?: boolean;
 }
 
 const MovieCardFeed = (props: MovieCardFeedProps) => {
-  const { movies, onLoadMore, isLoadingMore = false } = props;
+  const {
+    movies,
+    onLoadMore,
+    isLoadingMore = false,
+    isHorizontal = false,
+    ...rest
+  } = props;
+  const { top } = useSafeAreaInsets();
 
   const renderItem: ListRenderItem<Movie> = useCallback(
     ({ item }) => (
-      <View style={styles.listItemContainer}>
-        <Link href={`/movie/${item.id}`} asChild>
+      <View
+        style={[
+          styles.listItemContainer,
+          isHorizontal ? styles.horizontalListItemContainer : undefined,
+        ]}
+      >
+        <Link href={`/movie/${item.id}`} push asChild>
           <MovieCard movie={item} />
         </Link>
       </View>
@@ -33,12 +54,19 @@ const MovieCardFeed = (props: MovieCardFeedProps) => {
   return (
     <FlatList
       data={movies}
+      horizontal={isHorizontal}
       renderItem={renderItem}
       keyExtractor={keyExtractor}
-      numColumns={2}
+      numColumns={isHorizontal ? undefined : 2}
       contentInsetAdjustmentBehavior="automatic"
       onEndReached={onLoadMore}
-      contentContainerStyle={styles.listContentContainer}
+      contentContainerStyle={[
+        styles.listContentContainer,
+        isHorizontal
+          ? styles.horizontalListContentContainer
+          : { paddingTop: top + 32 },
+      ]}
+      style={isHorizontal ? styles.horizontalListStyle : undefined}
       ListFooterComponent={() => {
         if (isLoadingMore) {
           return (
@@ -53,20 +81,28 @@ const MovieCardFeed = (props: MovieCardFeedProps) => {
           );
         }
       }}
+      {...rest}
     />
   );
 };
 
 const styles = StyleSheet.create({
+  horizontalListStyle: { flexGrow: 0 },
   listContentContainer: {
     marginHorizontal: -4,
     gap: 8,
     paddingHorizontal: 8,
     paddingVertical: 8,
   },
+  horizontalListContentContainer: { paddingHorizontal: 4 },
   listItemContainer: {
     flex: 1,
     marginHorizontal: 4,
+  },
+  horizontalListItemContainer: {
+    flex: undefined,
+    width: 200,
+    aspectRatio: 1 / 1.5,
   },
   loadingItemContainer: {
     aspectRatio: 1 / 1.5,
