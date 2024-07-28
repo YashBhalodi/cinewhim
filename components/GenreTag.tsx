32e27523a8d2a57f4bc73813e9d0f8db9d49bbb8
yashbhalodi-toddleapp <yashbhalodi@toddleapp.com>
2024-07-28 18:45:03 +0530

    wip: genre tag ui component

---
+++ components/GenreTag.tsx
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Genre } from "@/api/model";
+import { ThemedText } from "./ThemedText";
+import { ThemedView } from "./ThemedView";
+
+interface GenreTagProps {
+  genre: Genre;
+}
+
+const GenreTag = (props: GenreTagProps) => {
+  const { genre } = props;
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText>{genre.name}</ThemedText>
+    </ThemedView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 8,
+  },
+});
+
+export default GenreTag;
