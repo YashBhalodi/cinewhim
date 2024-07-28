e35e3f93c9416d01831acd20bdfc983aa3b2552e
yashbhalodi-toddleapp <yashbhalodi@toddleapp.com>
2024-07-28 18:43:41 +0530

    fix: support size in rating view component

--- components/RatingView.tsx
+++ components/RatingView.tsx
@@ -1,11 +1,14 @@
-import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import { ThemedIcon } from "./ThemedIcon";
+import { StyleSheet } from "react-native";
+import { ThemedIcon, ThemedIconProps } from "./ThemedIcon";
+import { ThemedText, ThemedTextProps } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
-import { ThemedText } from "./ThemedText";
+
+type Size = "sm" | "md" | "lg";
 
 interface RatingViewProps {
   rating: number;
+  size: Size;
 }
 
 const RATING_STYLE_MAP = {
@@ -23,8 +26,26 @@ const RATING_STYLE_MAP = {
   },
 };
 
+const SIZE_MAP: Record<
+  Size,
+  { iconSize: ThemedIconProps["size"]; textType: ThemedTextProps["type"] }
+> = {
+  sm: {
+    iconSize: "xs",
+    textType: "body3",
+  },
+  md: {
+    iconSize: "sm",
+    textType: "body2",
+  },
+  lg: {
+    iconSize: "md",
+    textType: "body1",
+  },
+};
+
 const RatingView = (props: RatingViewProps) => {
-  const { rating } = props;
+  const { rating, size = "md" } = props;
 
   const ratingType = rating > 7 ? "good" : rating < 5 ? "bad" : "neutral";
 
@@ -32,10 +53,13 @@ const RatingView = (props: RatingViewProps) => {
     <ThemedView style={styles.container}>
       <ThemedIcon
         name="star"
-        size="xs"
+        size={SIZE_MAP[size].iconSize}
         variant={RATING_STYLE_MAP[ratingType].iconColor}
       />
-      <ThemedText type="body3" color={RATING_STYLE_MAP[ratingType].textColor}>
+      <ThemedText
+        type={SIZE_MAP[size].textType}
+        color={RATING_STYLE_MAP[ratingType].textColor}
+      >
         {rating.toPrecision(2)}
       </ThemedText>
     </ThemedView>
